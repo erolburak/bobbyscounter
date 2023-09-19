5b6064fb984c72e138339f632129b4f952f506ee
Burak Erol <140210017+erolburak@users.noreply.github.com>
2023-09-19 20:41:29 +0200

    cleanup (#61)

--- BobbysCounter/Presentation/SettingsView.swift
+++ BobbysCounter/Presentation/SettingsView.swift
@@ -35,88 +35,91 @@ struct SettingsView: View {
 				.padding()
 				.accessibilityIdentifier("DatePicker")
 
-				Group {
-					if counters.count > 1 {
-						Chart {
-							ForEach(counters) { counter in
-								LineMark(x: .value("Date",
-												   counter.date,
-												   unit: .day),
-										 y: .value("Count",
-												   counter.count))
-								.interpolationMethod(.monotone)
-								.lineStyle(StrokeStyle(lineWidth: 1,
-													   dash: [2]))
+				if counters.count > 1 {
+					Chart {
+						ForEach(counters) { counter in
+							LineMark(x: .value("Date",
+											   counter.date,
+											   unit: .day),
+									 y: .value("Count",
+											   counter.count))
+							.interpolationMethod(.monotone)
+							.lineStyle(StrokeStyle(lineWidth: 1,
+												   dash: [2]))
 
-								PointMark(x: .value("Date",
-													counter.date,
-													unit: .day),
-										  y: .value("Count",
-													counter.count))
-								.annotation(position: .topLeading,
-											spacing: 4,
-											overflowResolution: .init(x: .fit(to: .chart),
-																	  y: .fit(to: .chart))) {
-									if viewModel.showAnnotation(date: counter.date),
-									   let selectedPointMarkCounter = viewModel.selectedPointMarkCounter(counters: counters) {
-										VStack {
-											Text(selectedPointMarkCounter.count.description)
-												.font(.system(size: 100))
-												.minimumScaleFactor(0.01)
-												.lineLimit(1)
-												.opacity(0.25)
-												.padding(2)
-										}
-										.frame(width: 60,
-											   height: 60)
-										.background {
-											RoundedRectangle(cornerRadius: 4)
-												.fill(Color(uiColor: .systemBackground))
-												.shadow(color: .black,
-														radius: 2)
-										}
-										.overlay(alignment: .topTrailing) {
-											Text(selectedPointMarkCounter.date.toRelative)
-												.font(.system(size: 4))
-												.padding(2)
-										}
-										.padding(2)
+							PointMark(x: .value("Date",
+												counter.date,
+												unit: .day),
+									  y: .value("Count",
+												counter.count))
+							.annotation(position: .topLeading,
+										spacing: 4,
+										overflowResolution: .init(x: .fit(to: .chart),
+																  y: .fit(to: .chart))) {
+								if viewModel.showAnnotation(date: counter.date),
+								   let selectedPointMarkCounter = viewModel.selectedPointMarkCounter(counters: counters) {
+									VStack {
+										Text(selectedPointMarkCounter.count.description)
+											.font(.system(size: 100))
+											.minimumScaleFactor(0.01)
+											.lineLimit(1)
+											.opacity(0.25)
+											.padding(2)
 									}
+									.frame(width: 60,
+										   height: 60)
+									.background {
+										RoundedRectangle(cornerRadius: 4)
+											.fill(Color(uiColor: .systemBackground))
+											.shadow(color: .black,
+													radius: 2)
+									}
+									.overlay(alignment: .topTrailing) {
+										Text(selectedPointMarkCounter.date.toRelative)
+											.font(.system(size: 4))
+											.padding(2)
+									}
+									.padding(2)
 								}
 							}
 						}
-						.chartScrollableAxes(.horizontal)
-						.chartScrollPosition(x: $viewModel.chartScrollPosition)
-						.chartXVisibleDomain(length: viewModel.chartXVisibleDomainLength(counters: counters))
-						.chartXSelection(value: $viewModel.selectedPointMarkDate)
-						.chartXScale(range: .plotDimension(padding: 12))
-						.chartYScale(range: .plotDimension(padding: 12))
-						.foregroundStyle(.red)
-						.accessibilityIdentifier("Chart")
-						.onAppear {
-							Task {
-								withAnimation {
-									let date = viewModel.counterSelected.selectedDate
-									let dateMinusOne = Calendar.current.date(byAdding: DateComponents(day: -1),
-																			 to: date) ?? date
-									viewModel.chartScrollPosition = dateMinusOne
-								}
+					}
+					.chartScrollableAxes(.horizontal)
+					.chartScrollPosition(x: $viewModel.chartScrollPosition)
+					.chartXVisibleDomain(length: viewModel.chartXVisibleDomainLength(counters: counters))
+					.chartXSelection(value: $viewModel.selectedPointMarkDate)
+					.chartXScale(range: .plotDimension(padding: 12))
+					.chartYScale(range: .plotDimension(padding: 12))
+					.frame(height: 120)
+					.fixedSize(horizontal: false,
+							   vertical: true)
+					.padding(.horizontal)
+					.font(.system(size: 10))
+					.foregroundStyle(.red)
+					.accessibilityIdentifier("Chart")
+					.onAppear {
+						Task {
+							withAnimation {
+								let date = viewModel.counterSelected.selectedDate
+								let dateMinusOne = Calendar.current.date(byAdding: DateComponents(day: -1),
+																		 to: date) ?? date
+								viewModel.chartScrollPosition = dateMinusOne
 							}
 						}
-					} else {
-						Text("ChartDescription")
-							.multilineTextAlignment(.center)
-							.opacity(0.25)
-							.accessibilityIdentifier("ChartDescriptionText")
 					}
 				}
-				.font(.system(size: 10))
-				.frame(height: 120)
-				.fixedSize(horizontal: false,
-						   vertical: true)
-				.padding(.horizontal)
 
 				Spacer()
+			}
+			.overlay {
+				if counters.count < 2 {
+					ContentUnavailableView {
+						Label("EmptyCharts",
+							  systemImage: "chart.xyaxis.line")
+					} description: {
+						Text("EmptyChartsMessage")
+					}
+				}
 			}
 			.toolbar {
 				ToolbarItem(placement: .destructiveAction) {
@@ -182,7 +185,7 @@ struct SettingsView: View {
 				dismiss()
 			}
 		}
-		.presentationDetents([.fraction(0.4)])
+		.presentationDetents([.fraction(counters.count < 2 ? 0.6 : 0.4)])
 		.fontWeight(.bold)
 		.fontDesign(.monospaced)
 		.tint(.red)
